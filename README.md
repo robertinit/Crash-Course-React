c2a259ddcbf9d84eca1ba9a253805d6357af2dbd
robertinit@comcast.net <robertinit@comcast.net>
2019-09-28 13:50:22 -0600

    Passing Children

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 (Programming with Mosh)[https://www.youtube.com/channel/UCWv7vMbMWH4-V0ZXdmDpPBA]
 
-(React Tutorial - Learn React - React Crash Course \[2019\])[https://youtu.be/Ke90Tje7VS0?t=2451] Setting Attributes.
+(React Tutorial - Learn React - React Crash Course \[2019\])[https://www.youtube.com/watch?v=Ke90Tje7VS0&t=5070] Setting Attributes.
 (JavaScript for React Developers | Mosh)[https://www.youtube.com/watch?v=NCwa_xi0Uuc]
--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,19 +1,55 @@
 import React, { Component } from "react";
+// import { link } from "fs";
 
 class Counter extends Component {
   state = {
-    count: 0
+    value: this.props.value,
+    tags: ["tag1", "tag2", "tag3"]
+    // imageUrl: "https://picsum.photos/200"
+  };
+  //   constructor() {
+  //     super();
+  //     console.log("constructor", this);
+  //     this.handleIncrement = this.handleIncrement.bind(this);
+  //   }
+  styles = {
+    fontSize: 10,
+    fontWeight: "bold"
+  };
+  handleIncrement = product => {
+    console.log(product);
+    this.setState({ value: this.state.value + 1 });
   };
   render() {
     return (
-      <React.Fragment>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
-      </React.Fragment>
+      <div>
+        {/* <img src={this.state.imageUrl} alt="" /> */}
+        {/* <span style={this.styles} className="badge badge-primary m-2"> */}
+        {/* <span style={{ fontSize: 30 }} className="badge badge-primary m-2"> */}
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          //   onClick={this.handleIncrement}
+          onClick={() => this.handleIncrement({ id: 1 })} //product not defined
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        {/* <ul>
+          {this.state.tags.map(tag => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul> */}
+      </div>
     );
   }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+
   formatCount() {
-    const { count } = this.state;
+    const { value: count } = this.state;
     return count === 0 ? "Zero" : count;
   }
 }
